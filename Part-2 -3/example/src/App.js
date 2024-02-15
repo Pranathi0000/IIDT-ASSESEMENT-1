3ab3065275c0ac146ccc5966a4e4c17a4642286d
Doctor Pranathi <126224688+Pranathi0000@users.noreply.github.com>
2024-02-15 14:54:48 +0530

    Add files via upload

---
+++ Part-2 -3/example/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+
+
+function App() {
+  return (
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+  </Routes>
+  );
+}
+
+export default App;
