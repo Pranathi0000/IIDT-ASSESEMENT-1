c1dd553c558ae7475996e37088609175b0673f8f
Doctor Pranathi <126224688+Pranathi0000@users.noreply.github.com>
2024-02-15 14:40:03 +0530

    Add files via upload

---
+++ Part-2/example/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Greeting from './Components/Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my sample ReactApp</h1>
+      <Greeting name="IIDT"/>
+    </div>
+  );
+}
+
+export default App;
