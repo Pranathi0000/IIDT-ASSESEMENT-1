52bd50c5d93b5627312bbc2b7275e7d0d76c5691
Doctor Pranathi <126224688+Pranathi0000@users.noreply.github.com>
2024-02-15 14:21:05 +0530

    Add files via upload

---
+++ Part-1/script.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    // Get the paragraph element by its id
+    var paragraph = document.getElementById("paragraph");
+    // Set the color property to red
+    paragraph.style.color = "red";
+  }
